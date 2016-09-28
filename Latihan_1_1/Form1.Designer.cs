3e97815096ebd39d6258c25cd7085f3ed38a2fe6
chyntiawi <chyntiachyn7@gmail.com>
2016-09-28 22:25:01 +0700

    Latihan_1_1

---
+++ Latihan_1_1/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Latihan_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.maxdate = new System.Windows.Forms.TextBox();
+            this.mindate = new System.Windows.Forms.TextBox();
+            this.range = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.maxbar = new System.Windows.Forms.VScrollBar();
+            this.minbar = new System.Windows.Forms.VScrollBar();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(402, 335);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.maxdate);
+            this.tabPage1.Controls.Add(this.mindate);
+            this.tabPage1.Controls.Add(this.range);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.max);
+            this.tabPage1.Controls.Add(this.min);
+            this.tabPage1.Controls.Add(this.maxbar);
+            this.tabPage1.Controls.Add(this.minbar);
+            this.tabPage1.ImageIndex = 0;
+            this.tabPage1.Location = new System.Drawing.Point(4, 23);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(394, 308);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Search";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // maxdate
+            // 
+            this.maxdate.Location = new System.Drawing.Point(201, 85);
+            this.maxdate.Name = "maxdate";
+            this.maxdate.Size = new System.Drawing.Size(79, 20);
+            this.maxdate.TabIndex = 9;
+            this.maxdate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mindate
+            // 
+            this.mindate.Location = new System.Drawing.Point(201, 56);
+            this.mindate.Name = "mindate";
+            this.mindate.Size = new System.Drawing.Size(79, 20);
+            this.mindate.TabIndex = 9;
+            this.mindate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // range
+            // 
+            this.range.Location = new System.Drawing.Point(201, 28);
+            this.range.Name = "range";
+            this.range.Size = new System.Drawing.Size(31, 20);
+            this.range.TabIndex = 9;
+            this.range.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(114, 125);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2026, 9, 28, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2006, 9, 28, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            this.dateTimePicker1.Value = new System.DateTime(2016, 9, 28, 0, 0, 0, 0);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(185, 88);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(10, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = ":";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(185, 59);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(185, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Max Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(111, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Min Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Range";
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Location = new System.Drawing.Point(66, 132);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(13, 13);
+            this.max.TabIndex = 4;
+            this.max.Text = "0";
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Location = new System.Drawing.Point(22, 132);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(13, 13);
+            this.min.TabIndex = 2;
+            this.min.Text = "0";
+            // 
+            // maxbar
+            // 
+            this.maxbar.Location = new System.Drawing.Point(66, 31);
+            this.maxbar.Maximum = 19;
+            this.maxbar.Name = "maxbar";
+            this.maxbar.Size = new System.Drawing.Size(14, 97);
+            this.maxbar.TabIndex = 1;
+            this.maxbar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.maxbar_Scroll);
+            // 
+            // minbar
+            // 
+            this.minbar.Location = new System.Drawing.Point(21, 31);
+            this.minbar.Maximum = 19;
+            this.minbar.Name = "minbar";
+            this.minbar.Size = new System.Drawing.Size(14, 97);
+            this.minbar.TabIndex = 0;
+            this.minbar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.ImageIndex = 1;
+            this.tabPage2.Location = new System.Drawing.Point(4, 23);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(394, 308);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "search.png");
+            this.imageList1.Images.SetKeyName(1, "settings.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 359);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox maxdate;
+        private System.Windows.Forms.TextBox mindate;
+        private System.Windows.Forms.TextBox range;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.VScrollBar maxbar;
+        private System.Windows.Forms.VScrollBar minbar;
+    }
+}
+
---
+++ Latihan_1_1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            min.Text = minbar.Value.ToString();
+            range.Text = Math.Abs(minbar.Value + maxbar.Value).ToString();
+            DateTime label6 = new DateTime((DateTime.Today.Year - minbar.Value),DateTime.Today.Month,DateTime.Today.Day);
+            mindate.Text = label6.ToString("dd/MM/yyyy");
+            dateTimePicker1.MinDate = label6;
+        }
+
+        private void maxbar_Scroll(object sender, ScrollEventArgs e)
+        {
+            max.Text = maxbar.Value.ToString();
+            range.Text = Math.Abs(minbar.Value + maxbar.Value).ToString();
+            DateTime label7 = new DateTime((DateTime.Today.Year + maxbar.Value), DateTime.Today.Month, DateTime.Today.Day);
+            maxdate.Text = label7.ToString("dd/MM/yyyy");
+            dateTimePicker1.MaxDate = label7;
+        }
+    }
+}
